a83bf3076b22fa4606b71182afda956e79c637c6
Vulgofalcaoprethelas <lorenzoofernandes2@gmail.com.br>
2024-04-25 08:08:02 -0300

    okkkzuber

--- styles.js
+++ styles.js
@@ -1,60 +1,60 @@
-body{
-    font-family: Arial, Arial, Helvetica, sans-serif;
-    font-size: 22px;
-}
-h1,h2,h3,p{
-    color: aliceblue;
-    text-align: center;
-}
-img{
-    margin-left: 40px;
-    margin-right: 40px;
-    margin-top: 10px;
-}
-*{
-    margin: 0;
-    padding: 0;
-}
-#cabecalho{
-    height: 150px;
-    background-color: orange;
-    text-align: center;
-    width: 95%;
-    margin: 0 auto;
-}
-#menu{
-    height: 400px;
-    width: 400px;
-    background-color: greenyellow;
-    float:left;
-    margin-left: 40px;
-    margin-top: 10px;
-    margin-right: 40px;
-}
-#conteudo{
-    height: 500px;
-    width: 530px;
-    background-color: lightgray;
-    float: left;
-    margin-top: 10px;
-    margin-left: 40px;
-}
+// body{
+//     font-family: Arial, Arial, Helvetica, sans-serif;
+//     font-size: 22px;
+// }
+// h1,h2,h3,p{
+//     color: aliceblue;
+//     text-align: center;
+// }
+// img{
+//     margin-left: 40px;
+//     margin-right: 40px;
+//     margin-top: 10px;
+// }
+// *{
+//     margin: 0;
+//     padding: 0;
+// }
+// #cabecalho{
+//     height: 150px;
+//     background-color: orange;
+//     text-align: center;
+//     width: 95%;
+//     margin: 0 auto;
+// }
+// #menu{
+//     height: 400px;
+//     width: 400px;
+//     background-color: greenyellow;
+//     float:left;
+//     margin-left: 40px;
+//     margin-top: 10px;
+//     margin-right: 40px;
+// }
+// #conteudo{
+//     height: 500px;
+//     width: 530px;
+//     background-color: lightgray;
+//     float: left;
+//     margin-top: 10px;
+//     margin-left: 40px;
+// }
  
-#rodape{
-    height: 320px;
-    background-color: pink;
-    clear: both;
-    width: 95%;
-    margin: 0 auto;
-    text-align: center;
-}
+// #rodape{
+//     height: 320px;
+//     background-color: pink;
+//     clear: both;
+//     width: 95%;
+//     margin: 0 auto;
+//     text-align: center;
+// }
  
-#lado{
-    height: 500px;
-    width: 350px;
-    background-color: lightgreen;
-    float: left;
-    margin-left: 5px;
-    margin-top: 10px;
-    margin-block-end: 5px;
-}
+// #lado{
+//     height: 500px;
+//     width: 350px;
+//     background-color: lightgreen;
+//     float: left;
+//     margin-left: 5px;
+//     margin-top: 10px;
+//     margin-block-end: 5px;
+// }
